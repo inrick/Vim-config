7a6e11be65e11182827d46d8b802584a6d1d3771
Fabian Carlström <inrick@gmail.com>
2024-03-01 14:29:29 +0100

    Format lua file

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,12 +3,12 @@
 --------------------------
 
 vim.g.go_def_mapping_enabled = 0
-vim.g.go_fmt_command = 'goimports'
+vim.g.go_fmt_command = "goimports"
 vim.g.go_fmt_experimental = 1
 vim.g.go_doc_max_height = 40
 vim.g.go_code_completion_enabled = 0
 vim.g.go_highlight_diagnostic_errors = 0
-vim.g.NERDDefaultAlign = 'left'
+vim.g.NERDDefaultAlign = "left"
 
 vim.keymap.set("n",          "<F4>",             ":NvimTreeToggle<CR>",              { silent = true })
 vim.keymap.set("n",          "-",                ":NvimTreeFocus<CR>",               { silent = true })
@@ -29,12 +29,12 @@ vim.keymap.set({ "n", "v" }, "<leader>a:",       ":Tabularize /:\\zs/l0l1<CR>", 
 vim.keymap.set({ "n", "v" }, "<leader>a;",       ":Tabularize /;\\zs/l0l1<CR>",      { remap = true })
 vim.keymap.set({ "n", "v" }, "<leader>a<space>", ":Tabularize /\\S\\ \\zs/l0l1<CR>", { remap = true })
 
-require('colorizer').setup()
+require("colorizer").setup()
 
-local luasnip = require 'luasnip'
+local luasnip = require("luasnip")
 
 -- Automatic completion
-local cmp = require 'cmp'
+local cmp = require("cmp")
 cmp.setup({
   preselect = cmp.PreselectMode.None,
   snippet = {
@@ -47,94 +47,94 @@ cmp.setup({
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-6),
-    ['<C-f>'] = cmp.mapping.scroll_docs(6),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    --['<C-g>'] = cmp.mapping.abort(),
-    ['<C-g>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ['<PageUp>'] = cmp.mapping(function(fallback)
+    ["<C-b>"] = cmp.mapping.scroll_docs(-6),
+    ["<C-f>"] = cmp.mapping.scroll_docs(6),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    --["<C-g>"] = cmp.mapping.abort(),
+    ["<C-g>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<PageUp>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         for _ = 1, 6 do cmp.select_prev_item() end
       else
         fallback()
       end
     end),
-    ['<PageDown>'] = cmp.mapping(function(fallback)
+    ["<PageDown>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         for _ = 1, 6 do cmp.select_next_item() end
       else
         fallback()
       end
     end),
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
   }, {
-    { name = 'buffer' },
+    { name = "buffer" },
   }, {
-    { name = 'path' },
+    { name = "path" },
   })
 })
 
 -- Search and command completion
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'buffer' }
+    { name = "buffer" }
   },
 })
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = 'path' }
+    { name = "path" }
   }, {
-    { name = 'cmdline' }
+    { name = "cmdline" }
   })
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(
+local capabilities = require("cmp_nvim_lsp").default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
 -- LSP
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
     for key, fn in pairs({
-      ['<space>e']  = vim.diagnostic.open_float,
-      ['[d']        = vim.diagnostic.goto_prev,
-      [']d']        = vim.diagnostic.goto_next,
-      ['<space>q']  = vim.diagnostic.setloclist,
-      ['gD']        = vim.lsp.buf.declaration,
-      ['gd']        = vim.lsp.buf.definition,
-      ['<C-]>']     = vim.lsp.buf.definition,
-      ['gy']        = vim.lsp.buf.type_definition,
-      ['K']         = vim.lsp.buf.hover,
-      ['gi']        = vim.lsp.buf.implementation,
-      ['<C-k>']     = vim.lsp.buf.signature_help,
-      ['<space>f']  = function() vim.lsp.buf.format({ async = false }) end,
-      ['<space>rn'] = vim.lsp.buf.rename,
-      ['<space>ca'] = vim.lsp.buf.code_action,
-      ['gr']        = vim.lsp.buf.references,
+      ["<space>e"]  = vim.diagnostic.open_float,
+      ["[d"]        = vim.diagnostic.goto_prev,
+      ["]d"]        = vim.diagnostic.goto_next,
+      ["<space>q"]  = vim.diagnostic.setloclist,
+      ["gD"]        = vim.lsp.buf.declaration,
+      ["gd"]        = vim.lsp.buf.definition,
+      ["<C-]>"]     = vim.lsp.buf.definition,
+      ["gy"]        = vim.lsp.buf.type_definition,
+      ["K"]         = vim.lsp.buf.hover,
+      ["gi"]        = vim.lsp.buf.implementation,
+      ["<C-k>"]     = vim.lsp.buf.signature_help,
+      ["<space>f"]  = function() vim.lsp.buf.format({ async = false }) end,
+      ["<space>rn"] = vim.lsp.buf.rename,
+      ["<space>ca"] = vim.lsp.buf.code_action,
+      ["gr"]        = vim.lsp.buf.references,
     }) do
-      vim.keymap.set('n', key, fn, {
+      vim.keymap.set("n", key, fn, {
         buffer = bufnr, noremap = true, silent = false,
       })
     end
@@ -151,21 +151,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- LSP config
 local lspservers = {
-  'clangd', 'gopls', 'pyright', 'ruff_lsp',
+  "clangd", "gopls", "pyright", "ruff_lsp",
   rust_analyzer = {
     -- See https://rust-analyzer.github.io/manual.html
     settings = {
-      ['rust-analyzer'] = {
+      ["rust-analyzer"] = {
         assist = {
-          importGranularity = 'module',
-          importPrefix = 'self',
+          importGranularity = "module",
+          importPrefix = "self",
         },
         cargo = {
           allFeatures = true,
           loadOutDirsFromCheck = true,
         },
         --checkOnSave = {
-          --command = 'clippy',
+          --command = "clippy",
         --},
         completion = {
           autoimport = {
@@ -184,7 +184,7 @@ local lspservers = {
     settings = {
       Lua = {
         runtime = {
-          version = 'LuaJIT',
+          version = "LuaJIT",
         },
         workspace = {
           checkThirdParty = false,
@@ -200,10 +200,11 @@ local lspservers = {
     },
   },
 }
-local lspconfig = require 'lspconfig'
+
+local lspconfig = require("lspconfig")
 for k, v in pairs(lspservers) do
   local name, settings
-  if type(k) == 'number' then
+  if type(k) == "number" then
     name = v
   else
     name = k
@@ -219,29 +220,29 @@ for k, v in pairs(lspservers) do
 end
 
 -- LSP icons
-local lspkind = require 'lspkind'
+local lspkind = require("lspkind")
 cmp.setup {
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol_text',
+      mode = "symbol_text",
       maxwidth = 50,
     }),
   },
 }
 
-require('nvim-tree').setup({
+require("nvim-tree").setup({
   view = {
     width = 35,
   },
 })
 
-require('nvim-web-devicons').setup({
+require("nvim-web-devicons").setup({
   color_icons = false,
 })
 
-require('nvim-treesitter.configs').setup({
+require("nvim-treesitter.configs").setup({
   ensure_installed = {
-    'go', 'lua', 'c', 'python', 'rust',
+    "go", "lua", "c", "python", "rust",
   },
   highlight = {
     enable = true,
@@ -249,22 +250,22 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
-require('ibl').setup({
+require("ibl").setup({
   enabled = true,
   indent = {
-    char = '▏',
-    highlight = { 'IndentBlanklineIndent' },
+    char = "▏",
+    highlight = { "IndentBlanklineIndent" },
   },
   scope = {
     enabled = false,
-    char = '▎',
+    char = "▎",
   },
   exclude = {
     filetypes = {
-      'conf',
-      'haskell',
-      'make',
-      'ocaml',
+      "conf",
+      "haskell",
+      "make",
+      "ocaml",
     },
     buftypes = {},
   },
